f6fdb649f502a6f4213fd1c569f8ede54ea530bb
Muhammad Hassan Asim <hassan.asim@10pearls.com>
2021-11-08 21:54:06 +0500

    Setup initial base project

---
+++ Friendr/Friendr/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Friendr
+//
+//  Created by Hassan Asim on 08/11/2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ app: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = RootViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+}
---
+++ Friendr/Friendr/RootViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RootViewController.swift
+//  Friendr
+//
+//  Created by Hassan Asim on 08/11/2021.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+}
